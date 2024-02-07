b19084e9d4255c1d737cc51e944f9d139e4fccc0
OttPFC <ale.maz097@gmail.com>
2024-02-07 22:53:23 +0100

    ultimo commit g3

--- m1/s3/g3/ToDo list/script.js
+++ m1/s3/g3/ToDo list/script.js
@@ -1,33 +1,52 @@
-//aggiungo cose alla lista
-const invia = document.getElementById('aggiungi')
-invia.addEventListener('click',()=>{
-    let task = document.getElementById('task').value
-    let lista = document.getElementById('lista')
-    //let creaLista = document.createElement('ul')
-    let elemento = document.createElement('li')
-    elemento.innerText = task
-    lista.appendChild(elemento)
-    
-})
-//crea checkbox alla creazione dell'elemento
-invia.addEventListener('click',()=>{
-    let lista = document.getElementById('lista')
-    let element = document.createElement('input')
-    element.type = 'checkbox'
-    lista.appendChild(element)
-})
 
-//rimuovo dalla lista
-const rimuovi = document.getElementById('rimuovi')
-rimuovi.addEventListener('click',rimuoviDo)
-function rimuoviDo(){
-    let lista = document.getElementById('lista')
-    lista.removeChild(lista.lastChild)
-}
 
-//inserisco il nome della lista
+const aggiungi = document.getElementById('aggiungi');
+
 function inserisciNome(nome) {
     nome = prompt('Inserisci il nome della Task')
     let titolo = document.getElementById('titolo');
     titolo.innerText = nome;
+}
+//inserisciNome()
+
+aggiungi.addEventListener('click',function(){
+
+    const taskInput = document.getElementById('task')
+    let taskValue = taskInput.value
+
+    
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    li.appendChild(checkbox);
+
+    checkbox.addEventListener('click',function(){
+        if(checkbox.checked){
+            li.style.textDecoration = 'line-through';
+        }else{
+            li.style.textDecoration = 'none';
+        }
+    })
+
+
+
+    let rimuovi = document.createElement('button');
+    rimuovi.innerText = 'Rimuovi';
+    li.append(rimuovi);
+    rimuovi.addEventListener('click',function(){
+        li.remove();
+    });
+
+    let textContent = document.createTextNode(taskValue);
+    li.appendChild(textContent);
+
+    let ul = document.getElementById('lista')
+    ul.appendChild(li);
+
+    taskInput.value = '';
+});
+
+
+function spuntaCheck(){
+
 }
