17f622947aac95eec03a774cf0287e656e50ad06
OttPFC <ale.maz097@gmail.com>
2024-02-09 16:34:54 +0100

    commit finale

--- m1/s3/g5/D10.js
+++ m1/s3/g5/D10.js
@@ -527,7 +527,8 @@ function halfTree(n) {
 */
 
 function isItPrime(n){
-
+  if (n === 1 || n === 0) return false;
+  if (n === undefined) return 'Inserisci un numero!';
   for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false;
@@ -535,7 +536,7 @@ function isItPrime(n){
   }
   return true;
 }
-console.log(isItPrime(79));
+console.log(isItPrime());
 
 
 /* Questo array viene usato per gli esercizi. Non modificarlo. */
