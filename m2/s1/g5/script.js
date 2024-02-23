fb3c6ce3ba2edb9ba0a544b8567ffb80486d7c33
OttPFC <ale.maz097@gmail.com>
2024-02-23 18:08:16 +0100

    commit js scroll navbar

---
+++ m2/s1/g5/script.js
@@ -0,0 +1,21 @@
+
+let nav = document.getElementById('nav');
+let btn = document.getElementById('btn');
+
+function scrollTop() {
+    window.onscroll = function () {
+    if (document.body.scrollTop >= 250 || document.documentElement.scrollTop >= 250 ) {
+        nav.classList.remove("nav-default");
+        nav.classList.add("nav-colored");
+        btn.classList.remove("b-menu");
+        btn.classList.add("b-menu2");
+}else{
+    nav.classList.remove("nav-colored");
+    nav.classList.add("nav-default");
+    btn.classList.remove("b-menu2");
+        btn.classList.add("b-menu");
+}
+};
+}
+
+scrollTop();
