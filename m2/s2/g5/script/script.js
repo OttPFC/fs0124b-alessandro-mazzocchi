140d0b2c41f370a56ea327ca298683f0c63c314c
OttPFC <ale.maz097@gmail.com>
2024-03-03 10:57:12 +0100

    carosello

--- m2/s2/g5/script/script.js
+++ m2/s2/g5/script/script.js
@@ -1,18 +1,90 @@
-let splide = new Splide( '.splide', {
-    perPage: 3,
-    gap    : '2rem',
+/*
+  let splide = new Splide( '.splide', {
+    
+    perPage: 6,
     breakpoints: {
       640: {
         perPage: 2,
-        gap    : '.7rem',
+        
         height : '6rem',
       },
       480: {
         perPage: 1,
-        gap    : '.7rem',
+        
         height : '6rem',
       },
     },
   } );
   
   splide.mount();
+
+  let splide2 = new Splide( '.splide2', {
+    
+    perPage: 6,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        
+        height : '6rem',
+      },
+      480: {
+        perPage: 1,
+        
+        height : '6rem',
+      },
+    },
+  } );
+  
+  splide2.mount();
+
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  new Splide('#first-carousel', {
+    perPage: 6,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        
+        height : '6rem',
+      },
+      480: {
+        perPage: 1,
+        
+        height : '6rem',
+      },
+    },
+  }).mount();
+
+  
+  new Splide('#second-carousel', {
+    perPage: 6,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        
+        height : '6rem',
+      },
+      480: {
+        perPage: 1,
+        
+        height : '6rem',
+      },
+    },
+  }).mount();
+
+  new Splide('#third-carousel', {
+    perPage: 6,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        height : '6rem',
+      },
+      480: {
+        perPage: 1,
+        height : '6rem',
+      },
+    },
+  }).mount();
+});
