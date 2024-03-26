a093ca277298a70bbe4654f6ba6eb0ad29d80d47
OttPFC <ale.maz097@gmail.com>
2024-03-26 15:06:03 +0100

    esercizio g2

---
+++ m3/s2/g2/completo/src/app/Direttive/colori.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ColoriDirective } from './colori.directive';
+
+describe('ColoriDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ColoriDirective();
+    expect(directive).toBeTruthy();
+  });
+});
