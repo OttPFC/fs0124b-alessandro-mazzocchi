07e01fbf5658330d03cd3f33e454ea4804d073ac
OttPFC <ale.maz097@gmail.com>
2024-03-27 16:21:43 +0100

    g3

--- m3/s2/g3/esercizioProdotti/src/app/components/favorites/favorites.component.ts
+++ m3/s2/g3/esercizioProdotti/src/app/components/favorites/favorites.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServiceProductsService } from '../../services/service-products.service';
+import { iProducts } from '../../interfaces/products';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss'
 })
-export class FavoritesComponent {
+export class HomeComponent implements OnInit {
 
+  prod:iProducts[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: ServiceProductsService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      const productId = params.id;
+      console.log(productId);
+      
+      this.service.getProductById(productId).subscribe(res => {
+        if (res) {
+          this.prod = [res['products'][0]];
+          console.log(this.prod);
+        } 
+      });
+    });
+  }
 }
