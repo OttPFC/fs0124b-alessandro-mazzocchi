3957756fb96e520ec8ac41105643a3de3ff8195d
OttPFC <ale.maz097@gmail.com>
2024-03-28 15:55:49 +0100

    fix preferiti con subject

--- m3/s2/g4/esercizio/src/app/Model/foto.service.ts
+++ m3/s2/g4/esercizio/src/app/Model/foto.service.ts
@@ -1,47 +1,50 @@
 import { Injectable } from '@angular/core';
 import { iPhotos } from '../interfaces/photos';
 import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs/internal/Subject';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FotoService {
 
-  private apiUrl:string = "https://jsonplaceholder.typicode.com/photos"
-  photos:iPhotos[] = [];
+  private apiUrl: string = "https://jsonplaceholder.typicode.com/photos";
+  photos: iPhotos[] = [];
   photoSubject = new BehaviorSubject<iPhotos[]>([]);
   $photo = this.photoSubject.asObservable();
 
-  favourites:iPhotos[] = [];
-  favSubject = new Subject<iPhotos>();
+  favourites: iPhotos[] = [];
+  favSubject = new BehaviorSubject<iPhotos[]>([]);
   
-  $favourites = this.favSubject.asObservable().pipe(tap(p => {
-    this.favourites.push(p)
-  }))
+  $favourites = this.favSubject.asObservable().pipe(
+    tap(p => {
+      this.favourites = [...this.favourites, ...p];
+    })
+  );
 
-
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {
     this.getAllPhotos().subscribe(data => {
       this.photoSubject.next(data);
       this.photos = data;      
-    })
+    });
   }
 
-  getAllPhotos(){
+  getAllPhotos() {
     return this.http.get<iPhotos[]>(this.apiUrl);
   }
 
-  deletePhoto(id:number){
+  deletePhoto(id: number){
     return this.http.delete<iPhotos>(`${this.apiUrl}/${id}`)
-    .pipe(tap(()=> {
-      this.photos = this.photos.filter(photo => photo.id!== id)
+    .pipe(tap(() => {
+      this.photos = this.photos.filter(photo => photo.id !== id);
       this.photoSubject.next(this.photos);
-    }))
+    }));
   }
 
-  addToFavourites(product:iPhotos){
-    this.favSubject.next(product)
+  addToFavourites(photo: iPhotos){
+    const updatedFavourites = [...this.favourites, photo];
+    this.favSubject.next(updatedFavourites);
   }
+
+  
 }
--- m3/s2/g4/esercizio/src/app/components/favourites/favourites.component.ts
+++ m3/s2/g4/esercizio/src/app/components/favourites/favourites.component.ts
@@ -1,28 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { iPhotos } from '../../interfaces/photos';
 import { FotoService } from '../../Model/foto.service';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
-  styleUrl: './favourites.component.scss'
+  styleUrls: ['./favourites.component.scss']
 })
-export class FavouritesComponent {
+export class FavouritesComponent implements OnInit {
 
-  photos:iPhotos[] = []
+  photos: iPhotos[] = [];
 
-  constructor(private photoSvr:FotoService) {}
+  constructor(private photoSvr: FotoService) {}
 
-  ngOnInit() {
-    this.likeAtThis()
+  ngOnInit(){
+    this.likeAtThis();
   }
-
 
   likeAtThis(){
     this.photoSvr.$favourites.subscribe(photos => {
-      this.photos.push(photos)
-      console.log('MI piace');
-    });
+        this.photos = photos;  
+        console.log(this.photos);
+        console.log('Mi Piace'); 
+      }
+    );
   }
 
+  
 }
