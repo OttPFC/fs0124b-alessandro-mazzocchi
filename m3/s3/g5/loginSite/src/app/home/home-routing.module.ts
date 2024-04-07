0de3025f420142c61a4afdc71186a0f6bff6c9af
OttPFC <ale.maz097@gmail.com>
2024-04-07 17:14:44 +0200

    fix profile

--- m3/s3/g5/loginSite/src/app/home/home-routing.module.ts
+++ m3/s3/g5/loginSite/src/app/home/home-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { PriceComponent } from '../main-components/price/price.component';
 
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
