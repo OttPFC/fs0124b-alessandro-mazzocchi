7e724f6728ad838a848c6db2d08cd7a9443d06a5
OttPFC <ale.maz097@gmail.com>
2024-10-15 12:11:46 +0200

    change login

--- m3/s3/g5/loginSite/src/app/home/home.component.ts
+++ m3/s3/g5/loginSite/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { iLoginData } from '../interfaces/auth';
+import { iUsers } from '../interfaces/users';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +11,40 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private http:HttpClient){}
+
+
+  loginData:iLoginData = {
+    email: '',
+    password: ''
+  }
+
+  registerData:Partial<iUsers> = {}
+
+  showSignup = true
+
+  constructor(private http:HttpClient, private authSvc:AuthService, private router:Router){}
 
   ngOnInit(){
 
     this.http.get('http://localhost:3000/users').subscribe()
 
   }
+  signIn(){
 
+    this.authSvc.login(this.loginData)
+    .subscribe(data => {
+      this.router.navigate(['/dashboard'])
+    })
+console.log(this.loginData);
+
+  }
+
+signUp(){
+  this.authSvc.register(this.registerData)
+  .subscribe(data => {
+
+    this.showSignup = true
+
+  })
+}
 }
